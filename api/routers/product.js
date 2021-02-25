8903602c3d74ad3f1162719f9afae04df8ea7806
Rajib Miah <ahmedrajib942@gmail.com>
2021-02-25 05:25:31 -0800

    product api is set to run

--- api/routers/product.js
+++ api/routers/product.js
@@ -5,58 +5,85 @@ const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
    Product.find()
-   .exec()
-   .then(doc =>{
-      res.status(200).send(doc)
-   })
-   .catch(error =>{
-      res.status(404).send('documents not found')
-   })
-   res.status(200).send('handling get reqeust form products')
+      .exec()
+      .then(doc => {
+         if (doc) {
+            res.status(200).send(doc)
+         } else {
+            res.status(404).send('No entry found')
+         }
+      })
+      .catch(error => {
+         res.status(404).send('documents not found')
+      })
 })
 
 router.post('/', (req, res, next) => {
-    const product = Product({
-       _id : mongoose.Types.ObjectId(),
-       name : req.body.name,
-       price : req.body.price
-    })
-    product.save().
-    then(
-       (result)=>{
-         console.log(result)
-       }
-    )
-    .catch(error=>console.log(error))
+   const product = Product({
+      _id: mongoose.Types.ObjectId(),
+      name: req.body.name,
+      price: req.body.price
+   })
+   product.save().
+      then(
+         (result) => {
+            console.log(result)
+         }
+      )
+      .catch(error => console.log(error))
 
-    res.status(201).json({
+   res.status(201).json({
       msg: "Product was created",
       product
-    })
+   })
 })
 
 router.get('/:productId', (req, res, next) => {
-   const id =   req.params.productId
+   const id = req.params.productId
    Product.findById(id)
-   .exec()
-   .then(doc =>{
-       console.log(doc)
-       res.status(200).send(doc)
-   })
-   .catch(error =>{
-       console.log(error)
-       res.status(500).json({error:error})
-   })
+      .exec()
+      .then(doc => {
+         if (doc.length > 0) {
+            res.status(200).send(doc)
+         } else {
+            res.status(404).send('No valid entry found from provided id')
+         }
+      })
+      .catch(error => {
+         console.log(error)
+         res.status(500).json({ error: error })
+      })
 })
 
 router.patch('/productId', (req, res, next) => {
-   const id =   req.params.productId
-   req.status(200).send(id)
+   const id = req.params.productId
+   const updateOps = {}
+   for(const ops of req.body){
+      updateOps[ops.propName] = ops.value
+   }
+   Product.updateOne({ _id: id }, {
+      $set: {updateOps}
+   })
+   .exec()
+   .then(result =>{
+      res.status(200).send(result)
+   })
+   .catch(err =>{
+      res.status(500).send(err)
+   })
 })
 
 router.delete('/productId', (req, res, next) => {
-   const id =   req.params.productId
-   req.status(200).send(id)
+   const id = req.params.productId
+   Product.remove({ _id: id })
+      .exec()
+      .then(result => {
+         res.status(200).send(result)
+      })
+      .catch(err => {
+         res.send(404).send('No valid entry found from provided id')
+      })
+
 })
 
 module.exports = router
