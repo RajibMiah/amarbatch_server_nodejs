7d8a05516a4e32f817a3e4d50b61a0a40bd55663
Rajib Miah <ahmedrajib942@gmail.com>
2021-03-08 15:21:46 -0800

    user delete profile

--- api/routers/user.js
+++ api/routers/user.js
@@ -9,31 +9,54 @@ router.post('/signin', (req, res, next) => {
 })
 
 router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      res.status(500).json({
-        error: err
-      })
-    } else {
-      const user = new User({
-        _id: mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash
-      })
-      user.save()
-        .then(result => {
-          res.status(201).json({
-            message: 'user created'
-          })
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        res.status(409).json({
+          message: "email exist"
         })
-        .catch(err => {
-          res.status(500).json({
-            error: err
-          })
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            res.status(500).json({
+              error: err
+            })
+          } else {
+            const user = new User({
+              _id: mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            })
+            user.save()
+              .then(result => {
+                res.status(201).json({
+                  message: 'user created'
+                })
+              })
+              .catch(err => {
+                res.status(500).json({
+                  error: err
+                })
+              })
+          }
         })
+      }
     }
-  })
+    )
+})
 
+router.delete('/:userId', (req, res, next) => {
+  User.remove({ id: req.body.userId })
+    .exec()
+    .then(result => {
+      res.status(500).json({
+        message: "user deleted"
+      })
+    })
+    .catch(err => {
+      error: err
+    })
 })
 
 module.exports = router
