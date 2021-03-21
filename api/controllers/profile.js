64889b2fc6370a53897a8bfa8500bf838239a474
Rajib Miah <ahmedrajib942@gmail.com>
2021-03-20 20:01:17 -0800

    profile controller set

--- api/controllers/profile.js
+++ api/controllers/profile.js
@@ -2,7 +2,25 @@
 const mongoose = require('mongoose')
 const User = require('../models/user')
 
-exports.profileInfo = (req, res, next) => {
-  const profile = User.findOne({ id: req.body.userId })
-  console.log("profile", profile)
+exports.profileInfo = async (req, res, next) => {
+  try {
+    const profile = await User.findById(req.params.profileId )
+    if (profile) {
+      res.status(201).json({
+        message: 'user profile fatched',
+        code: 'SUCCESS',
+        data: {
+          userName : profile.userName,
+          email: profile.email,
+          phoneNo: profile.phoneNo,
+          bio : profile.bio,
+          education: profile.education
+        }
+      })
+    }
+  } catch (error)  {
+       res.status(500).json({
+        Error: error,
+    })
+  }
 }
--- api/routers/profile.js
+++ api/routers/profile.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const checkAuth = require('../middleware/check-auth')
 const profileController = require("../controllers/profile")
 
-router.get('/:profileId', checkAuth.jwtAuth, profileController.profileInfo)
+router.get('/:profileId', checkAuth.jwtAuth , profileController.profileInfo)
 
 module.exports = router
